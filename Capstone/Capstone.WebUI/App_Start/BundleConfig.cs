4f99a842d893a5dd9289ac61c1e9ed274721c482
Marian <katsrneet@gmail.com>
2014-05-21 13:44:56 -0700

    still playing
    determination wins sometimes

--- Capstone/Capstone.WebUI/App_Start/BundleConfig.cs
+++ Capstone/Capstone.WebUI/App_Start/BundleConfig.cs
@@ -16,25 +16,18 @@ namespace Capstone.WebUI
                         "~/Scripts/bootstrap-modal.js"
                         ));
 
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
-
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                "~/Scripts/jquery.unobtrusive*",       
-                "~/Scripts/jquery.validate*"));
+                        "~/Scripts/jquery.unobtrusive*",
+                        "~/Scripts/jquery.validate*"));
 
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
-
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                    "~/Content/site.css",
+                    "~/Content/bootstrap.css"
+                    ));
+
             bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
                         "~/Content/themes/base/jquery.ui.core.css",
                         "~/Content/themes/base/jquery.ui.resizable.css",
--- Capstone/Capstone.WebUI/Global.asax.cs
+++ Capstone/Capstone.WebUI/Global.asax.cs
@@ -16,6 +16,7 @@ namespace Capstone.WebUI
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
         }
     }
 }
