8018b1a01c1bc92a40b901e1cd1190e72b64ef7f
mpolicicchio <aliquisme@gmail.com>
2014-05-31 20:51:39 -0700

    Start Leader Board
    Started the process of creating a leader board for the main page.

---
+++ Capstone/Capstone.WebUI/Controllers/LeaderBoardController.cs
@@ -0,0 +1,36 @@
+﻿using Capstone.WebUI.Domain.Abstract;
+using Capstone.WebUI.Domain.Concrete;
+using Capstone.WebUI.Domain.Entities;
+using Capstone.WebUI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Capstone.WebUI.Controllers
+{
+    public class LeaderBoardController : Controller
+    {
+        FormInterface fRepo;
+
+        // The default constructor is called by the framework
+        public LeaderBoardController()
+        {
+            fRepo = new FormRepository();
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        /* public Form GetComplete()
+        {
+            var db = new ApplicationDbContext();
+            var leaderForms = (from f in db.Forms
+                                where f.complete = true
+                                select f).ToList<Form>();
+        }*/
+    }
+}
