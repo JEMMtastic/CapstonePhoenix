cfdbb5261263046611ed1f8614dcd324517f0d1b
Marian <katsrneet@gmail.com>
2014-06-08 14:53:51 -0700

    Time saving now
    Think it has to do with the way it's entered. Still working on it. But
    if you put it in exactly like you are supposed to, it works

--- Capstone/Capstone.WebUI/Controllers/CRUDController.cs
+++ Capstone/Capstone.WebUI/Controllers/CRUDController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Web;
